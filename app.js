b4c35e31c24732fbf640f277dde49b86b7142e82
dave-horton <160142213+dave-horton@users.noreply.github.com>
2024-02-15 09:10:24 -0600

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This is a practice project!'));
+app.get('/', (req, res) => res.send('This is a great practice project!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
